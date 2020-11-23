2349071ba2ff3542636271b90b8e134a0ab950e3
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-11-23 23:24:33 +0900

    jQueryの導入

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,15 @@ class ProfilesController < ApplicationController
 
   def show
     @account = current_user.account_name
+    @profile = current_user.profile
   end
+
+  def edit
+    @profile = current_user.prepare_profile
+  end
+
+
+
 
   
 end

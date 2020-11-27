20a5d1645fc862813935712f9231ce1202245adc
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-11-28 01:17:10 +0900

    twitterのシェアボタン追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -15,7 +16,13 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import $ from 'jquery'
 import axios from 'axios'
+// import $ from 'jquery'
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   $('.slider-img').on('click', () => {
+//     window.alert('aaaaaaaaaa')
+//   })
+// })
 
 

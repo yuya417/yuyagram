974a6d3ec944a37d5523d1f0ed5b49fa9c48e4d1
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-12-11 14:47:25 +0900

    no message

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -4,26 +4,42 @@ RSpec.describe 'Articles', type: :request do
   let!(:user) { create(:user) }
   let!(:articles) { create_list(:article, 5, user: user) }
   
-  # describe 'GET /articles' do
-  #   it '302ステータスが返ってくる' do
-  #     get articles_path
-  #     expect(response).to have_http_status(302)
-  #   end
-  # end
+  describe 'GET /articles' do
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+      it '200ステータスが返ってくる' do
+        get articles_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    it 'ログイン画面に遷移する' do
+      get articles_path
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
 
   describe 'POST /articles' do
     context 'ログインしている場合' do
       before do
         sign_in user
-        # @article = FactoryBot.build(:article)
-        # @article.images = fixture_file_upload('app/assets/images/Ellipse.png')
       end
-
       it '記事が保存される' do
         article_params = FactoryBot.attributes_for(:article)
         post articles_path({article: article_params})
         expect(response).to have_http_status(302)
         expect(Article.last.content).to eq(article_params[:content])
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it 'ログイン画面に遷移する' do
+        article_params = FactoryBot.attributes_for(:article)
+        post articles_path({article: article_params})
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
